5db8eefd4e184e438dc1f743727b2ccaafe13710
cmavrido <cmavrido@csd.auth.gr>
2020-12-06 19:13:14 +0200

    t

--- src/src/Questions.java
+++ src/src/Questions.java
@@ -1,19 +1,25 @@
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
 
     public class Questions {
 
         private ArrayList<Question> questions;
 
         public Questions() {
-            questions = <new Question("In what movie does Robin Williams play the role of a divorced father posing as an elderly British nanny to spend more time with his children? ",
-                             new String[]={"Mr. Mom", "Toys", "Tootsie", "Mrs. Doubtfire"},4),
-                        new Question("Which of the following metals is liquid at room temperature?",
-                             new String[]={"Mercury","Magnesium","Cobalt","Potassium",1)>;
+            questions = new ArrayList<Question>();
 
+
+        questions.add(new Question("In what movie does Robin Williams play the role of a divorced father posing as an elderly British nanny to spend more time with his children? ",
+                new String[]{"Mr. Mom", "Toys", "Tootsie", "Mrs. Doubtfire"},4));
+        questions.add(new Question("Which of the following metals is liquid at room temperature?",
+                new String[]{"Mercury", "Magnesium", "Cobalt", "Potassium"},1));
+
+
+            }
         }
         Random randomGenerator = new Random();
         int index = randomGenerator.nextInt(questions.size());
         Question question = questions.get(index);
-    }
+    //}
 }
